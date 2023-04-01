aa691cc995fe97021f95ddad28829451c0a349b0
Nicolas Goyon <nicolas.goyon@etu.umontpellier.fr>
2023-04-01 11:41:33 +0000

    CodeSandBox commit

--- src/drag-and-drop-example/App.tsx
+++ src/drag-and-drop-example/App.tsx
@@ -8,21 +8,13 @@ import {DndContext} from '@dnd-kit/core';
 import {Draggable} from '../components/Dnd/Draggable';
 import {Droppable} from '../components/Dnd/Droppable';
 let count = 0;
-let itemsTab : JSX.Element[] = [];
 
 export default function App() {
     let d : String[] = []
     let dZone :String[]  = []
     const [items, setItems] = useState(d);
     const [parents, setParents] = useState(dZone);
-    useEffect(() => {
-        majItemsTab();
-        // console.log("items :");
-        // console.info(items);
-        // console.log("parents :");
-        // console.info(parents);
-      }, [items, parents, setItems, setParents]);
-    majItemsTab();
+
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -35,9 +27,11 @@ export default function App() {
         <div className="flex flex-row w-full gap-5">
             <div className="flex flex-col w-full border-2 gap-5 min-h-40 p-10 bg-primary">
                 <Droppable id="droppable-1" style={{width: "100%", height: "100%"}}>
-                    {itemsTab.map((item, index) => {
+                    {items.map((item, index) => {
                         if (parents[index] === "droppable-1") {
-                            return item;
+                            return (<Draggable key={index} id={index}>
+                                {item}
+                            </Draggable>);
                         }
                     })}
                 </Droppable>
@@ -45,18 +39,22 @@ export default function App() {
             
             <div className="w-full border-2 h-40 bg-light">
                 <Droppable id="droppable-2" style={{width: "100%", height: "100%"}} >
-                    {itemsTab.map((item, index) => {
+                    {items.map((item, index) => {
                         if (parents[index] === "droppable-2") {
-                            return item;
+                            return (<Draggable key={index} id={index}>
+                                {item}
+                            </Draggable>);
                         }
                     })}
                 </Droppable>
             </div>
             <div className="w-full border-2 h-40 bg-warning">
                 <Droppable id="droppable-3" style={{width: "100%", height: "100%"}}>
-                    {itemsTab.map((item, index) => {
+                    {items.map((item, index) => {
                         if (parents[index] === "droppable-3") {
-                            return item;
+                            return (<Draggable key={index} id={index}>
+                                {item}
+                            </Draggable>);
                         }
                     })}
                 </Droppable>
@@ -78,14 +76,6 @@ export default function App() {
         count++;
         setItems(items => [...items, 'Item'+count]);
         setParents(parents => [...parents, 'droppable-1']);
-    }
-
-    function majItemsTab() {
-        itemsTab = items.map((item, index) => (
-            <Draggable key={index} id={index}>
-                {item}
-            </Draggable>
-        ))
     }
     
     function handleDragEnd(over: any) {
