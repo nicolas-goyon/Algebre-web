eb464f8a56791fdf4c5f7740bde2e543dfb19208
Nicolas Goyon <nicolas.goyon@etu.umontpellier.fr>
2023-04-02 10:58:34 +0000

    Encapsulation des elements ok

--- src/components/Playground/Demo.tsx
+++ src/components/Playground/Demo.tsx
@@ -1,17 +1,18 @@
-import React, {useState} from 'react';
-import { Projection } from "../../assets/classes/Projection";
-import { Ensemble } from "../../assets/classes/Ensemble";
-import {DndContext} from '@dnd-kit/core';
+import React, {useEffect, useState} from 'react';
+// import { Projection } from "../../assets/classes/Projection";
+// import { Ensemble } from "../../assets/classes/Ensemble";
+import {DndContext, pointerWithin} from '@dnd-kit/core';
 import {Draggable} from '../Dnd/Draggable';
 import {Droppable} from '../Dnd/Droppable';
 import Renommage from './Renomage';
+import Selection from './Selection';
 import { TrashIcon } from '@heroicons/react/24/outline'
 
 
-let c = ["id", "test"]
-let e = new Ensemble("Etudiant")
-let f = new Ensemble("Test")
-let p = new Projection(c, e)
+// let c = ["id", "test"]
+// let e = new Ensemble("Etudiant")
+// let f = new Ensemble("Test")
+// let p = new Projection(c, e)
 
 // let chmpRen = {
 //     "id": "id2",
@@ -30,20 +31,38 @@ let p = new Projection(c, e)
 
 
 export default function Demo(prop: any) {
-    let d : {id : String, parent: String}[] = []
-    let itemsList : JSX.Element[] = [];
+    let d : {id : String, parent: String, type: String}[] = []
     
     const [count, setCount] = useState(0);
     const [items, setItems] = useState(d);
+    let dropListBase : String[] = [ "droppable-1", "droppable-2", "droppable-3"];
     
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <DndContext onDragEnd={handleDragEnd}>
+            <button onClick={addRenommage}
+                className="flex items-center gap-2 px-3 py-1.5 text-sm text-indigo-600 duration-150 bg-indigo-50 rounded-lg hover:bg-indigo-100 active:bg-indigo-200"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M12 5.25a.75.75 0 01.75.75v5.25H18a.75.75 0 010 1.5h-5.25V18a.75.75 0 01-1.5 0v-5.25H6a.75.75 0 010-1.5h5.25V6a.75.75 0 01.75-.75z" clipRule="evenodd" />
+                </svg>
+                Renommage
+            </button>
+            <button onClick={addSelection}
+                className="flex items-center gap-2 px-3 py-1.5 text-sm text-indigo-600 duration-150 bg-indigo-50 rounded-lg hover:bg-indigo-100 active:bg-indigo-200"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                    <path fillRule="evenodd" d="M12 5.25a.75.75 0 01.75.75v5.25H18a.75.75 0 010 1.5h-5.25V18a.75.75 0 01-1.5 0v-5.25H6a.75.75 0 010-1.5h5.25V6a.75.75 0 01.75-.75z" clipRule="evenodd" />
+                </svg>
+                Selection
+            </button>
+
+
+            <DndContext onDragEnd={handleDragEnd} collisionDetection={pointerWithin} >
                 <section className="py-16 w-full h-full">
                     <div className="flex flex-row w-full gap-5">
                         <div className="flex flex-col w-full border-2 gap-5 min-h-40 bg-primary">
-                            <Droppable id="droppable-1" style={{width: "100%", height: "100%"}}>
+                            <Droppable id={dropListBase[0]} style={{width: "100%", height: "100%"}}>
                                 <div className='p-10 h-full w-100'>
                                 {items.map((item, index) => {
                                     if (items[index].parent === "droppable-1") {
@@ -55,7 +74,7 @@ export default function Demo(prop: any) {
                         </div>
                         
                         <div className="w-full border-2 gap-5 min-h-40 bg-light">
-                            <Droppable id="droppable-2" style={{width: "100%", height: "100%"}} >
+                            <Droppable id={dropListBase[1]} style={{width: "100%", height: "100%"}} >
                                 <div className='p-10 h-full w-100'>
                                 {items.map((item, index) => {
                                     if (items[index].parent  === "droppable-2") {
@@ -64,14 +83,13 @@ export default function Demo(prop: any) {
                                 })}
                                 </div>
                             </Droppable>
-                            {/* Mettre la poubelle en bas à droite de la zone en taille 20 par 20 et un contour rouge */}
                            
                         </div>
-                        <div className="justify-end w-20 h-20">
-                            <div className="gap-5 min-h-40 bg-warning-200 border-danger border-2">
-                                <Droppable id="droppable-3" style={{width: "100%", height: "100%"}}>
+                        <div className="justify-end w-30 h-30">
+                            <div className="gap-5 border-danger rounded-md border-2">
+                                <Droppable id={dropListBase[2]} style={{width: "100%", height: "100%"}}>
                                     {/* Logo heroicon de taille 50 par 50 avec des bords rond en rouge (danger) */}
-                                    <TrashIcon className="h-20 w-20 text-red-500" />
+                                    <TrashIcon className="h-full w-full text-red-500" />
                                     
                                 </Droppable>
                             
@@ -83,53 +101,110 @@ export default function Demo(prop: any) {
                 </section>
                         
             </DndContext>
-            <button onClick={addItem}>Add item</button>
+            
         </div>
     );
     
     function makeItem(index: number, type: String): JSX.Element {
-        let idDrop = "droppable-"+(index+5);
+        
+        let idDrop : String = "";
+        if( type != "Ensemble") {
+            idDrop = "droppable-" + index + "-" + 1;
+        }
+        let block = <Renommage/>
+        switch (items[index].type) {
+            case "Renommage":
+                block =  (
+                    <Renommage/>
+                );
+                break;
+            case "Selection":
+                block = (
+                    <Selection/>
+                );
+                break;
+        }
+        
+        
         // créer un item renommage et permet d'avoir une zone de drop dans l'item renommage
         return (
             <Draggable key={index} id={index.toString()}>
-                <div className="flex flex-row w-full gap-5">
-                    <div className="flex flex-col w-full border-2 gap-5 min-h-40 bg-primary">
-                    <Renommage />
-                        <Droppable id={idDrop} style={{width: "100%", height: "100%"}}>
-                            <div className='p-10 h-full w-100'>
-                                {/* faire en sorte que l'item soit draggable dans la zone de drop */}
-                                {items.map((item, index) => {
-                                    if (items[index].parent === idDrop) {
-                                        return makeItem(index, idDrop);
-                                    }
-                                })}
+                <div className="flex flex-col border-2 bg-light-50" style={{minWidth:"10em"}}>
+                {block}
 
-                            </div>
-                        </Droppable>
-                    </div>
+                {type != "Ensemble" &&
+                    <Droppable id={idDrop} style={{width: "100%", height: "100%", backgroundColor:"grey"}}>
+                        <div className='pl-4 py-3 w-100' style={{"minHeight": "6em"}} >
+                            {/* faire en sorte que l'item soit draggable dans la zone de drop */}
+                            {items.map((item, index2) => {
+                                if (items[index2].parent === idDrop) {
+                                    return makeItem(index2, idDrop);
+                                }
+                            })}
+
+                        </div>
+                    </Droppable>
+                }
                 </div>
             </Draggable>
         );
     }
 
-    function addItem() {
+    function addRenommage() {
         setCount(count + 1);
-        setItems(items => [...items, {id: "item " + count, parent: "droppable-1"}]);
+        setItems(items => [...items, {id: "item " + count, parent: "droppable-1", type: "Renommage"}]);
+    }
+    function addSelection() {
+        setCount(count + 1);
+        setItems(items => [...items, {id: "item " + count, parent: "droppable-1", type: "Selection"}]);
     }
     
-    function handleDragEnd(over: any) {
-        if (over) {
-            const idBlock = over.active.id;
-            const idZone = over.over.id;
+    function handleDragEnd(event: any) {
+        const {over, active} = event;
+        if (event) {
+            const idBlock = active.id;
+            const idZone = over.id;
+            
+            if (!dropListBase.includes(over.id)) {
+                // Compter le nombre d'item dans la zone de drop
+                let count = countItem(idZone);
+                // Si il y a plus de 1 item dans la zone de drop alors on ne peut pas déposer l'item
+                // Vérifier que la zone ciblé n'est pas lui même
+                const drop = idZone;
+                const idParent = drop.split("-")[1]; // droppable-1-1 => droppable 1 2 
+
+                console.log("idParent : " + idParent + " idBlock : " + idBlock)
+                
+                if (count > 0 || idParent == idBlock) {
+                    return;
+                }
+            }
+
             setItems(items => {
                 const newItems = [...items];
-                newItems[idBlock] = {id: items[idBlock].id, parent: idZone};
+                newItems[idBlock] = {id: items[idBlock].id, parent: idZone, type: items[idBlock].type};
                 return newItems;
             });
-            if (over.over.id === 'droppable-3') {
+            if (over.id === 'droppable-3') {
                 console.log("suppression : " + idBlock)
                 setItems(items => items.filter((item, index) => index !== parseInt(idBlock)));
             }
         }
+    }
+
+    // Fonction qui compte le nombre d'item dans une zone de drop
+    function countItem(id: String) {
+        return searchItem(id).length;
+    }
+
+    // Fonction qui permet de chercher un item dans une zone de drop
+    function searchItem(id: String) {
+        let itemList:{id:String, parent:String}[] = [];
+        items.map((item, index) => {
+            if (items[index].parent === id) {
+                itemList.push(items[index]);
+            }
+        })
+        return itemList;
     }
 }
--- src/components/Playground/Selection.tsx
+++ src/components/Playground/Selection.tsx
@@ -1,35 +1,11 @@
-import React, { useState } from 'react';
-import MathJax from 'react-mathjax';
-import {DndContext} from '@dnd-kit/core';
-import {Droppable} from '../Dnd/Droppable';
+import React from 'react';
+import { MathJax } from 'better-react-mathjax';
 
 export default function Selection(prop: any) {
-    const latexString = "\\sigma_{name='Dupond'}( Student )"
-    const [parent, setParent] = useState(null);
-
-    // return (
-    //     <DndContext onDragEnd={handleDragEnd}>
-    //       {!parent ? draggable : null}
-    //       <Droppable id="droppable">
-    //         {parent === "droppable" ? draggable : 'Drop here'}
-    //       </Droppable>
-    //     </DndContext>
-    //   );
-    
-      function handleDragEnd(over:any) {
-        setParent(over ? over.id : null);
-      }
+    const latexString = "$\\sigma_{name='Dupond'}$"
     return (
-        <DndContext onDragEnd={handleDragEnd}>
-        <Droppable id="droppable">
-            <div className="border-solid pointer-events-none select-none border-2 bg-[#F9e4db] border-sky-500  rounded-md w-100px min-h-100px p-4">
-                <h1>Selection</h1>
-                <MathJax.Provider>
-                    <MathJax.Node formula={latexString} />
-                </MathJax.Provider>
-            </div>
-            {'Drop here'}
-        </Droppable>
-        </DndContext>
+        <div className="border-solid pointer-events-none select-none border-2 border-light-900  rounded-md w-100">
+            <MathJax inline dynamic>{latexString}</MathJax>
+        </div>
     )
 }
