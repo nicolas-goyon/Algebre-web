33f1b2c66c7a7e3b218e4e2c06eb4b00c1d82a60
Nicolas Goyon <nicolas.goyon@etu.umontpellier.fr>
2023-04-02 11:39:39 +0000

    Ajout des fichiers du commit précédent n'étant pas passés

---
+++ src/assets/classes/Noeuds.tsx
@@ -0,0 +1,76 @@
+import React from "react"
+import { Renommage, Selection, Projection, Ensemble } from "../../components/Playground/NoeudsComponents"
+
+export enum Noeuds {
+    // Noeud de base 
+    Noeud = "Noeud",
+    Ensemble = "Ensemble",
+    
+    // Noeuds avec 1 enfant
+    Renommage = "Renommage",
+    Selection = "Selection",
+    Projection = "Projection",
+    
+    // Noeuds avec 2 enfants
+    Union = "Union",
+    Produit = "Produit",
+    Intersection = "Intersection",
+    Difference = "Difference",
+    
+    // Jointures
+    // Jointure = "Jointure",
+
+}
+export class NoeudsGet{
+    static getNoeuds(noeud: String): Noeuds{
+        switch(noeud){
+            case "Noeud":
+                return Noeuds.Noeud
+            case "Ensemble":
+                return Noeuds.Ensemble
+            case "Renommage":
+                return Noeuds.Renommage
+            case "Selection":
+                return Noeuds.Selection
+            case "Projection":
+                return Noeuds.Projection
+            case "Union":
+                return Noeuds.Union
+            case "Produit":
+                return Noeuds.Produit
+            case "Intersection":
+                return Noeuds.Intersection
+            case "Difference":
+                return Noeuds.Difference
+            default:
+                return  Noeuds.Noeud
+        }
+    }
+
+    static getJSXElement(noeud: Noeuds): JSX.Element{
+        switch(noeud){
+            case Noeuds.Noeud:
+                // return <Noeud />
+                return <div></div>
+            case Noeuds.Ensemble:
+                return <Ensemble />
+            case Noeuds.Renommage:
+                return <Renommage />
+            case Noeuds.Selection:
+                return <Selection />
+            case Noeuds.Projection:
+                return <Projection />
+            // case Noeuds.Union:
+            //     return <Union />
+            // case Noeuds.Produit:
+            //     return <Produit />
+            // case Noeuds.Intersection:
+            //     return <Intersection />
+            // case Noeuds.Difference:
+            //     return <Difference />
+            default:
+                // return <Noeud />
+                return <div></div>
+        }
+    }
+}
---
+++ src/components/Playground/NoeudsComponents.tsx
@@ -0,0 +1,10 @@
+export {default as Ensemble} from "./Ensemble";
+
+export {default as Renommage} from "./Renommage";
+export {default as Selection} from "./Selection";
+export {default as Projection} from "./Projection";
+
+// export {default as Intersection} from "./Intersection";
+// export {default as Difference} from "./Difference";
+// export {default as Union} from "./Union";
+// export {default as Produit} from "./Produit";
